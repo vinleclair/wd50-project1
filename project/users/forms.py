ce7d60a318383ccb45409f82d94e2375064ec01b
Vincent Leclair <vinleclair@gmail.com>
2019-03-06 17:03:10 -0500

    Add password reset via email link

--- project/users/forms.py
+++ project/users/forms.py
@@ -11,3 +11,9 @@ class RegisterForm(FlaskForm):
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email(), Length(min=6, max=40)])
     password = PasswordField('Password', validators=[DataRequired()])
+
+class EmailForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(min=6, max=40)])
+
+class PasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
