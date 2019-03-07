3291bfbdc99a9ce8298aedda86b9ee1d803994c3
Vincent Leclair <vinleclair@gmail.com>
2019-03-07 18:33:33 -0500

    Add custom error pages

--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,5 @@
 # imports
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
@@ -31,3 +31,19 @@ from project.books.views import books_blueprint
 # register the blueprints
 app.register_blueprint(users_blueprint)
 app.register_blueprint(books_blueprint)
+
+#custom error pages
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(403)
+def page_not_found(e):
+    return render_template('403.html'), 403
+
+
+@app.errorhandler(410)
+def page_not_found(e):
+    return render_template('410.html'), 410
